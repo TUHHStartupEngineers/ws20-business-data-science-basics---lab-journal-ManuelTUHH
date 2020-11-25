3807ff7cad50f2cf162aed26aef552f472de0d12
ManuelTUHH <74919302+ManuelTUHH@users.noreply.github.com>
2020-11-25 19:08:44 +0100

    Challenge 1.4

--- build_website.R
+++ build_website.R
@@ -15,7 +15,7 @@ rmarkdown::render_site(encoding = 'UTF-8')
 ###### THIS WILL ONLY WORK IF YOU HAVE STATICRYPT INSTALLED VIA NPM ######
 ##########################################################################
 
-system( paste0("staticrypt ./docs/journal.html ", Sys.getenv("LAB_KEY")) )
+system( paste0("staticrypt ./docs/journal.html ", Sys.setenv(LAB_KEY = "Data1Science2For3Days")) )
 
 # Remove and rename resulting files
 file.remove("./docs/journal.html")
